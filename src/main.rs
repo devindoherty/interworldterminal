e45f8673bd25e9ecdaf65c62929f930604b18ebb
Devin Doherty <devincdoherty@gmail.com>
2022-05-13 21:58:39 -0400

    Update files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::env;
+use std::fs;
 use std::io; 
+use std::io::Read;
 use std::io::Write; 
 
 mod statistics;
@@ -18,30 +21,6 @@ const TITLE: &str = r"
 |__| |__| \__|     |__|     |_______|| _| `._____|   \__/  \__/     \______/  | _| `._____||_______||_______/ 
                                                                                                                ";
 
-const CRAWL: &str = 
-"
-              .   88 years ago, a war between the six worlds  .        .     .
-                 of the Zolar star system threatenend the lives     .  .
-     .       .  of billions of sentient beings, and lead to the            .
-.        .     destruction of entire planets.                           .
-   .            On one side, the Inner System Alliance opposing    .      .
-             the Outer Rim Powers - the reasoning for the war 
-          . myriad but also superficial, both sides suffered losses .   .
-   .       almost unimaginable. Vast fleets, overwhelming armies, and        .  .
-.      .  death reigned for ten long years, with no hope in sight.     .             .
-    .      Until, eventually, with the dead too numerous to count, the   .    .
-.      war was brought to an end with the signing of the Armstice. Many    .
-    . observers in the Six Worlds thought the peace only temporary -
-     with a second war merely on the horizon.                                    .
-       To that end, the Alliance and the Rim came together to form an org-   .
- . anization dedicated to the prospect of promoting peace and cooperation
-  between all of the major worlds of the Zolar star system. For 88 years,
- this Interworld Service has maintained a fragile peace. Until now.
-    As the captain of the Interworld Service Starship Worthwhile Endeavour,
-charged with preventing a second Interworld War, you must decide how to best
-prevent another conflict from embroiling the star system in bloodshed once
-again. However, mysterious powers on the edges of known space have machinations
-all their own...      \n";
 
 struct GameState
 {
@@ -53,6 +32,8 @@ struct GameState
 
 fn main()
 {
+    let crawl: String = fs::read_to_string("ascii_art.txt").expect("Error reading file!");
+
     print!("{}[2J", 27 as char); // Clear terminal screen
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Set cursor to Row 1, Column 1
     let mut game_state = GameState
@@ -86,7 +67,9 @@ fn main()
             let main_selection = prompt("Selection: ".to_string());
             if main_selection == 'n'
             {
-                println!("{}", CRAWL);
+                println!("{}", crawl);
+                pause();
+                clear_screen();
                 game_state.state = 1;
             }
             if main_selection == 'q'
@@ -148,6 +131,12 @@ fn chargen(game_state: &mut GameState)
             println!("Command represents leadership ability and charisma.\nTactical represents lethality and skill in combat.\nOperations represents scientific knowledge and technical knowhow.\n");
         }
 
+        // Clears screen
+        // std::process::Command::new("clear").status().unwrap(); // Linux Command 
+        // std::process::Command::new("cls").status().unwrap(); // Windows Command
+        clear_screen();
+
+
         // println!("The value of choice: {}", choice);
     }
     // let life_path == false;
@@ -177,4 +166,29 @@ fn prompt(query: String) -> char
             Err(_) => continue,
         };
     }
+}
+
+fn clear_screen()
+{
+    if cfg!(target_os = "linux") || cfg!(target_os = "macos")
+    {
+        std::process::Command::new("clear").status().unwrap();
+    }
+    if cfg!(target_os = "windows")
+    {
+        std::process::Command::new("cls").status().unwrap();
+    }
+}
+
+fn pause() 
+{
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
 }
